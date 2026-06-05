312b853b22e664436b651af9116ca5c3c3c6643a
Erik Gabriel <erikgab10@gmail.com>
2026-06-04 22:46:45 -0400

    🗃️ feat(repository): adiciona repositório e configura conexão com MongoDB

---
+++ src/main/java/br/com/erikdev/workshopmongodb/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.erikdev.workshopmongodb.repository;
+
+import br.com.erikdev.workshopmongodb.domain.UsuarioEntity;
+import br.com.erikdev.workshopmongodb.dto.UsuarioDto;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends MongoRepository<UsuarioEntity, String> {
+}
