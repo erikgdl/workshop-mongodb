f1b4f8edd413f1cd229191ebbc00a088a4d0ed58
Erik Gabriel <erikgab10@gmail.com>
2026-06-04 22:47:52 -0400

    ⚙️ feat(service): implementa regras de negócio para busca e criar usuários

---
+++ src/main/java/br/com/erikdev/workshopmongodb/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package br.com.erikdev.workshopmongodb.service;
+
+import br.com.erikdev.workshopmongodb.domain.UsuarioEntity;
+import br.com.erikdev.workshopmongodb.dto.UsuarioDto;
+import br.com.erikdev.workshopmongodb.exception.ObjectNotFoundException;
+import br.com.erikdev.workshopmongodb.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public List<UsuarioDto> getFindAll() {
+        List<UsuarioEntity> usuarioEntities = usuarioRepository.findAll();
+        return usuarioEntities.stream()
+                .map(UsuarioDto::new)
+                .toList();
+    }
+
+    public UsuarioEntity getById(String id) throws ObjectNotFoundException {
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new  ObjectNotFoundException("Usuario não encontrado"));
+    }
+
+    public UsuarioEntity create(UsuarioDto usuarioDto) {
+        return usuarioRepository.save(UsuarioEntity.builder()
+                .id(usuarioDto.getId())
+                .nome(usuarioDto.getNome())
+                .email(usuarioDto.getEmail())
+                .build());
+    }
+}
